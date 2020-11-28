7df68344bbe190398aa42b0ef093a2cd1ff899da
Douglas Barlow <d.barlow@laposte.net>
2020-11-28 17:22:35 +0100

    added more getters and setters

--- application/models/Model_info.php
+++ application/models/Model_info.php
@@ -7,7 +7,6 @@ class Model_info extends CI_Model {
 
         }
 
-
         public function get_apropos()
         {
                  $query = $this->db->get('apropos');
@@ -24,6 +23,27 @@ class Model_info extends CI_Model {
         public function get_experiences()
         {
                 $query = $this->db->get('experiences');
+                return $query->result_array();
+
+        }
+        
+        public function get_veille()
+        {
+                $query = $this->db->get('veille');
+                return $query->result_array();
+
+        }
+        
+        public function get_contact()
+        {
+                $query = $this->db->get('contact');
+                return $query->result_array();
+
+        }
+        //------
+        public function set_contact($contact)
+        {
+                $query = $this->db->set('contact');
                 return $query->result_array();
 
         }
